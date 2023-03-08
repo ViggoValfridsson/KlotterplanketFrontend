79c86e4efe68942f7e36b6c749b5f682ee286a33
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-03-08 14:06:59 +0100

    created footer and header component. Created home page file

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>Klotterplanket</h1>
+        <button type="button">Create post</button>
+      </header>
+    </>
+  );
+};
+
+export default Header;
