59bc4a1e0a00db8fed48736630f6eba736412703
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-03-14 14:28:06 +0100

    additional modal styling

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -11,13 +11,11 @@ const Modal = ({ setOpenModal }) => {
   return (
     <div className={classes.modal_background} onClick={(e) => handleClickOutside(e)}>
       <div className={classes.modal_container}>
-        <div className={classes.title_close_btn}>
-          <button type="button" onClick={() => setOpenModal(false)} className="btn">
-            <AiOutlineClose className={classes.close_icon} />
-          </button>
-        </div>
         <div className={classes.modal_title}>
           <h2>Create Post</h2>
+          <button type="button" onClick={() => setOpenModal(false)} className={`${classes.title_close_btn} btn`}>
+            <AiOutlineClose className={classes.close_icon} />
+          </button>
         </div>
         <div className={classes.modal_body}>
           <form className={classes.form} action="">
